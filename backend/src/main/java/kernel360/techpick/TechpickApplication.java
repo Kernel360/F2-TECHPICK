a1f2581d3704433b227db983ea6233be4da85a8f
kimminkyeu <minkyeki@student.42seoul.kr>
2024-09-12 17:33:44 +0900

    feat: backend initial setup

---
+++ backend/src/main/java/kernel360/techpick/TechpickApplication.java
@@ -0,0 +1,13 @@
+package kernel360.techpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechpickApplication.class, args);
+	}
+
+}
