6eca3f9a9135d70f8c08cdf8bb080b8954e7408e
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-09-12 16:03:12 +0900

    chore: 테스트용 컴포넌트 삭제

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
 import styles from './page.module.css';
-import { Come } from '@/components/Test';
+
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Come />
+
       <main className={styles.main}>
         <Image
           className={styles.logo}
