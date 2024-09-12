c34f0999dfe0d4c6a896429ee1219a006ebbc114
PSH <gyak404@gmail.com>
2024-09-12 23:27:59 +0900

    feat : backend pr 빌드봇 테스트 (fail)

--- backend/src/main/java/kernel360/techpick/TechpickApplication.java
+++ backend/src/main/java/kernel360/techpick/TechpickApplication.java
@@ -8,6 +8,7 @@ public class TechpickApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		asdfzxcvasdf;
 		SpringApplication.run(TechpickApplication.class, args);
 	}
 
