69db6075e60424e53aaa80b93c3a4f1b060523f0
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-09-13 11:40:11 +0900

    fix: 파일명 오타 수정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { ClientProvider } from './ClientPrivider';
+import { ClientProvider } from './ClientProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
