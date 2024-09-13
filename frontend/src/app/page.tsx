55c9cc9c960c5416dfff5a02de9bd7771e92b09f
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-09-13 10:46:52 +0900

    feat: 깃허브 액션 테스트용 PR (#4)
    * chore: 세팅중인 상태
    
    * fix: vanilla-extract 설정 변경
    
    * fix: storybook 실행 안되는 오류 해결
    
    @chromatic-com/storybook이 문제였음.
    
    * chore: storybook tset 확인
    
    * chore: eslint 예외 추가
    
    * chore: eslint, prettier 설정
    
    * chore: DS_Store ignore
    
    * chore: husky 테스트
    
    * chore: husky 적용 중
    
    * chore: husky 적용
    
    * chore: 테스트용 컴포넌트 삭제
    
    * chore: jest 적용
    
    * chore: ts를 devDependencies로 이동
    
    * chore: sample test 파일 삭제
    
    * chore: add files to gitignore
    
    * chore: vscode setting 추가
    
    * chore: playwright example test code 삭제
    
    * chore: github action 테스트를 위한 싶패 테스트
    
    * chore: github action 테스트를 위한 성공 테스트
    
    * chore: vanilla-extract storybook 적용
    
    * chore: 프로젝트 세팅을 위한 필요없는 파일 없애기
    
    * chore: playwright yarn script 추가
    
    * chore: react-hot-toast 적용
    
    ---------
    
    Co-authored-by: Jason <enigsuss@gmail.com>

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import toast from 'react-hot-toast';
+
+const notify = () => toast('Here is your toast.');
+
+export default function Home() {
+  return (
+    <div>
+      <h1>This is main page</h1>
+      <button onClick={notify}>aa</button>
+    </div>
+  );
+}
