7c2923da378102d8b64af00c1b1bc2dda59a7623
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-09-19 16:50:44 +0900

    Revert "feat: 모노레포 분리 (#27)"
    This reverts commit aee282f54c44b04fa1c4a6047d7520444ae9da7c.

--- frontend/linkpick/next.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
-const withVanillaExtract = createVanillaExtractPlugin();
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = withVanillaExtract(nextConfig);
