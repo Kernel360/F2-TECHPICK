a46c594c638d55871e2b1f349b66ff5147ef6c58
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-09-23 14:29:57 +0900

    feat: css 파일 제거

--- frontend/techpick-extension/src/App.tsx
+++ frontend/techpick-extension/src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
 import { CurrentTabInfo } from './CurrentTabInfo';
 
 function App() {
--- frontend/techpick-extension/src/chorme-extension/background.ts
+++
@@ -1,7 +0,0 @@
-// chrome.runtime.onInstalled.addListener(() => {
-//   console.log('Extension installed');
-// });
-
-// chrome.bookmarks.onCreated.addListener((_id, bookmark) => {
-//   console.log('Bookmark created:', bookmark);
-// });
