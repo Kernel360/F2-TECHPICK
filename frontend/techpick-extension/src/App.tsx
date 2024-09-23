33a7057f3f3ccc966fd9d749f732bdb7749f6604
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-09-23 15:35:03 +0900

    feat: react-query provider 설정

--- frontend/techpick-extension/src/App.tsx
+++ frontend/techpick-extension/src/App.tsx
@@ -1,8 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routers/index.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
