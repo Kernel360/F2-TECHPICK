5c9c6b2acc0e8adcb6b2cc2d67eabcf3c7b4ebca
PSH <gyak404@gmail.com>
2024-09-23 11:18:09 +0900

    feat : Jwt token provider 구현

---
+++ backend/src/main/java/kernel360/techpick/core/config/JwtUtil.java
@@ -0,0 +1,59 @@
+package kernel360.techpick.core.config;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import kernel360.techpick.core.feature.user.User;
+
+// 패키지 위치에 대한 고민 필요
+@Component
+public class JwtUtil {
+
+	@Value("${spring.jwt.issuer}")
+	private String issuer;
+	@Value("${spring.jwt.secret}")
+	private String secret;
+
+	/*
+		userId만 토큰에 포함됨
+	 */
+	public String getToken(User user, Date expiry) {
+		return Jwts.builder()
+			.setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+			.setIssuer(issuer)
+			.setIssuedAt(new Date())
+			.setExpiration(expiry)
+			.claim("id", user.getId())
+			.signWith(SignatureAlgorithm.HS256, secret)
+			.compact();
+	}
+
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parser()
+				.setSigningKey(secret)
+				.parseClaimsJws(token);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public Long getUserId(String token) {
+		var claims = getClaims(token);
+		return claims.get("id", Long.class);
+	}
+
+	private Claims getClaims(String token) {
+		return Jwts.parser()
+			.setSigningKey(secret)
+			.parseClaimsJws(token)
+			.getBody();
+	}
+}
