7d280ffafc65aeee44b56f666a89741a87fe2166
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-09-24 17:33:14 +0900

    build: build시에는 빌드를 한번하게 변경

--- frontend/techpick-extension/vite.config.ts
+++ frontend/techpick-extension/vite.config.ts
@@ -8,14 +8,14 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    watch: {
-      include: 'src/**',
-      exclude: 'node_modules/**',
-    },
     rollupOptions: {
       input: {
-        background: resolve(__dirname, 'src/chorme-extension/background.ts'),
+        background: resolve(__dirname, 'src/chrome-extension/background.ts'),
         popup: resolve(__dirname, './index.html'),
+        contentscript: resolve(
+          __dirname,
+          './src/chrome-extension/contentscript.ts'
+        ),
       },
       output: {
         entryFileNames: '[name].js',
@@ -26,7 +26,7 @@ export default defineConfig({
   plugins: [
     react(),
     viteStaticCopy({
-      targets: [{ src: './src/chorme-extension/manifest.json', dest: '.' }],
+      targets: [{ src: './src/chrome-extension/manifest.json', dest: '.' }],
     }),
     vanillaExtractPlugin(),
     tsconfigPaths(),
