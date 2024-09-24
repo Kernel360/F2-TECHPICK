00488eca430fc8b197ec5bfb3b0218dccc978891
PSH <gyak404@gmail.com>
2024-09-24 16:31:17 +0900

    feat : oauth2 구현

---
+++ backend/src/main/java/kernel360/techpick/core/auth/Filter/TokenAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package kernel360.techpick.core.auth.Filter;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kernel360.techpick.core.feature.user.Role;
+import kernel360.techpick.core.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+	private final JwtUtil jwtUtil;
+	private static final String ACCESS_TOKEN_KEY = "access_token";
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+		String token = getTokenFromCookie(request);
+
+		if (jwtUtil.validateToken(token)) {
+			var authentication = convertToAuthentication(token);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
+
+		filterChain.doFilter(request, response);
+	}
+
+	private String getTokenFromCookie(HttpServletRequest request) {
+		var cookies = request.getCookies();
+		if (cookies != null) {
+			for (var cookie : cookies) {
+				if (cookie.getName().equals(ACCESS_TOKEN_KEY)) {
+					return cookie.getValue();
+				}
+			}
+		}
+		return null;
+	}
+
+	private Authentication convertToAuthentication(String token) {
+
+		Long id = jwtUtil.getUserId(token);
+		Role role = jwtUtil.getRole(token);
+
+		return new UsernamePasswordAuthenticationToken(
+			id,
+			token,
+			List.of(new SimpleGrantedAuthority(role.toString()))
+		);
+	}
+}
---
+++ backend/src/main/java/kernel360/techpick/core/auth/model/OAuth2UserInfo.java
@@ -0,0 +1,44 @@
+package kernel360.techpick.core.auth.model;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import kernel360.techpick.core.feature.user.Role;
+import kernel360.techpick.core.feature.user.SocialType;
+import lombok.Getter;
+
+public class OAuth2UserInfo implements OAuth2User {
+
+	@Getter
+	private final SocialType provider;
+	private final Map<String, Object> attributes;
+
+	public OAuth2UserInfo(String provider, Map<String, Object> attributes) {
+		this.provider = SocialType.providerIdOf(provider);
+		this.attributes = attributes;
+	}
+
+	@Override
+	public Map<String, Object> getAttributes() {
+		return attributes;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority(Role.ROLE_USER.toString()));
+	}
+
+	@Override
+	public String getName() {
+		return attributes.get("name").toString();
+	}
+
+	public String getEmail() {
+		return attributes.get("email").toString();
+	}
+}
