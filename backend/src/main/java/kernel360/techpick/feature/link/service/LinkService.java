0e0224120896c07b685216b0eb40dd1076a0401f
PSH <gyak404@gmail.com>
2024-09-27 01:47:04 +0900

    refactor: Transactional 어노테이션 추가

--- backend/src/main/java/kernel360/techpick/feature/link/service/LinkService.java
+++ backend/src/main/java/kernel360/techpick/feature/link/service/LinkService.java
@@ -3,6 +3,7 @@ package kernel360.techpick.feature.link.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import kernel360.techpick.core.exception.feature.link.ApiLinkException;
 import kernel360.techpick.core.model.link.Link;
@@ -27,6 +28,7 @@ public class LinkService {
 	 * @param request {@code LinkRequest}
 	 * @throws ApiLinkException [LI-002] 이미 존재하는 링크(URL)
 	 */
+	@Transactional
 	public void createLink(LinkRequest request) throws ApiLinkException {
 
 		Link link = linkMapper.createLink(request);
@@ -41,6 +43,7 @@ public class LinkService {
 		return linkRepository.existsByUrl(url);
 	}
 
+	@Transactional(readOnly = true)
 	public LinkResponse getLinkById(Long id) throws ApiLinkException {
 
 		Link link = linkRepository.findById(id).orElseThrow(ApiLinkException::LINK_NOT_FOUND);
@@ -48,6 +51,7 @@ public class LinkService {
 		return linkMapper.createLinkResponse(link);
 	}
 
+	@Transactional(readOnly = true)
 	public LinkResponse getLinkByUrl(String url) throws ApiLinkException {
 
 		Link link = linkRepository.findByUrl(url).orElseThrow(ApiLinkException::LINK_NOT_FOUND);
@@ -55,6 +59,7 @@ public class LinkService {
 		return linkMapper.createLinkResponse(link);
 	}
 
+	@Transactional(readOnly = true)
 	public List<LinkResponse> getLinkAll() {
 
 		List<Link> linkList = linkRepository.findAll();
@@ -64,6 +69,7 @@ public class LinkService {
 			.toList();
 	}
 
+	@Transactional
 	public void deleteLinkById(Long id) throws ApiLinkException {
 
 		Link targetLink = linkRepository.findById(id).orElseThrow(ApiLinkException::LINK_NOT_FOUND);
