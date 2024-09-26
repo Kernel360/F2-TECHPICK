39020b33c259db8e07377d86d255c67b1576cb0b
Jason <enigsuss@gmail.com>
2024-09-26 21:19:44 +0900

    chore: 불필요한 index 제거

--- frontend/techpick/public/image/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as IcProfile } from './ic_user.svg';
-export { default as IcDirectory } from './ic_directory.svg';
-export { default as IcSort } from './ic_sort.svg';
-export { default as IcSearch } from './ic_search.svg';
-export { default as IcDownArrow } from './ic_arrow_down.svg';
-export { default as IcRightArrow } from './ic_arrow_right.svg';
-export { default as IcFolder } from './ic_folder.svg';
-export { default as IcDocument } from './ic_doc.svg';
