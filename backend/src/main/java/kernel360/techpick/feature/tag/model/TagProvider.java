a683c4703f0234e7ccffc33e9a18b30574276798
PSH <gyak404@gmail.com>
2024-09-29 18:23:31 +0900

    refactor: userTagList 제공 메소드 정렬기준에 따라 분리 - OrderByTagOrder, OrderByTagId

--- backend/src/main/java/kernel360/techpick/feature/tag/model/TagProvider.java
+++ backend/src/main/java/kernel360/techpick/feature/tag/model/TagProvider.java
@@ -1,9 +1,7 @@
 package kernel360.techpick.feature.tag.model;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
@@ -30,16 +28,12 @@ public class TagProvider {
 		return tagRepository.findById(id).orElseThrow(ApiTagException::TAG_NOT_FOUND);
 	}
 
-	public List<Tag> findAllByUserId(Long userId) {
-		return tagRepository.findAllByUserIdOrderByTagOrder(userId);
+	public List<Tag> findAllByUserIdOrderByTagId(Long userId) {
+		return tagRepository.findAllByUserIdOrderById(userId);
 	}
 
-	public Map<Long, Tag> getTagMapByUserId(Long userId) {
-
-		Map<Long, Tag> tagMap = new HashMap<>();
-		tagRepository.findAllByUserId(userId)
-			.forEach(tag -> tagMap.put(tag.getId(), tag));
-		return tagMap;
+	public List<Tag> findAllByUserIdOrderByTagOrder(Long userId) {
+		return tagRepository.findAllByUserIdOrderByTagOrder(userId);
 	}
 
 	public boolean existsByUserIdAndName(Long id, String name) throws ApiTagException {
