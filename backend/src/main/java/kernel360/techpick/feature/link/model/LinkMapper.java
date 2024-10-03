a7b3e6f0c78656ee311542432eb5b69347b710f9
sangwonsheep <sangwonsheep@gmail.com>
2024-10-03 16:34:08 +0900

    refactor: link mapper에 imageUrl 추가

--- backend/src/main/java/kernel360/techpick/feature/link/model/LinkMapper.java
+++ backend/src/main/java/kernel360/techpick/feature/link/model/LinkMapper.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import kernel360.techpick.core.model.link.Link;
 import kernel360.techpick.feature.link.service.dto.LinkRequest;
 import kernel360.techpick.feature.link.service.dto.LinkResponse;
+import kernel360.techpick.feature.pick.service.dto.PickCreateRequest;
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -15,12 +16,19 @@ public class LinkMapper {
 		return Link.create(linkRequest.title(), linkRequest.description(), linkRequest.url());
 	}
 
+	public Link createLink(PickCreateRequest pickCreateRequest) {
+		return Link.create(pickCreateRequest.linkRequest().url(), pickCreateRequest.linkRequest().title(),
+			pickCreateRequest.linkRequest().description(),
+			pickCreateRequest.linkRequest().imageUrl());
+	}
+
 	public LinkResponse createLinkResponse(Link link) {
 		return new LinkResponse(
 			link.getId(),
 			link.getUrl(),
 			link.getTitle(),
-			link.getDescription()
+			link.getDescription(),
+			link.getImageUrl()
 		);
 	}
 }
