03f7ca2f9f31975445d41fa10b96af2cff2d6ce3
kimminkyeu <minkyeki@student.42seoul.kr>
2024-10-04 02:02:25 +0900

    :recycle: refactor: 구현체 코드 정리

--- backend/src/main/java/kernel360/techpick/feature/folder/service/parser/internal/StructureParserImpl.java
+++ backend/src/main/java/kernel360/techpick/feature/folder/service/parser/internal/StructureParserImpl.java
@@ -20,16 +20,6 @@ public class StructureParserImpl implements StructureParser {
 	private static final Long PARENT_OF_ROOT = null;
 	private final Gson parser = new Gson();
 
-	// NOTE: 쓸것만 검증한다.
-	// 근데 어차피 보낼때 이걸 들고 있다가 그대로 데이터 삽입해야 한다.
-	// 그럴려면 프론트 전용 node를 메모리에 들고 있어야 하는데 이게 말이 안됨.
-	// 전혀 내가 안쓸 값을 들고 있을 이유가 없음.
-	// 그런데, 프론트가 보낼 때 id가 없으면, 그건 검증을 백에서 못함. 보내줄때 오류 발생 밖에 안됨.
-	// 백엔드는 반드시 유효한 정보만 가지고 있어야 함.
-	// 따라서 검증이 필수
-	// 즉 잘못 보내진 값은 받아서도 안됨.
-
-
 	private List<StructureNode> flatNodesRecur(JsonArray array, Long parentFolderId) {
 
 		List<StructureNode> nodes = new ArrayList<>();
