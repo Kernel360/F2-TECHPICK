e43ccaef424eab38a074eb04b20d12dde7539ab1
kimminkyeu <minkyeki@student.42seoul.kr>
2024-10-05 22:02:30 +0900

    :recycle: refactor: 테스트 케이스 작성 모두 완료

--- backend/src/test/java/kernel360/techpick/folder/service/parser/ParserTest.java
+++ backend/src/test/java/kernel360/techpick/folder/service/parser/ParserTest.java
@@ -4,14 +4,13 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import kernel360.techpick.feature.folder.exception.ApiFolderException;
-import kernel360.techpick.feature.folder.service.parser.internal.StructureNode;
-import kernel360.techpick.feature.folder.service.parser.StructureParser;
+import kernel360.techpick.feature.folder.service.directory.internal.RelationalNode;
+import kernel360.techpick.feature.folder.service.directory.ClientDirectory;
 
 public class ParserTest {
 
@@ -28,25 +27,20 @@ public class ParserTest {
 	 *       3. children이 배열이 아닌 경우
 	 * */
 
-	StructureParser structureParser;
-
-	@BeforeEach
-	public void beforeEach() {
-		structureParser = new StructureParser();
-	}
-
 	@Test
 	@DisplayName("간단한 구조 테스트")
 	public void parse_json_string_test() {
 		// given
 		String json = ParserTestCase.CASE_OK_SIMPLE;
 		// when
-		List<StructureNode> structure = structureParser.parseFolderStructure(json);
+		ClientDirectory clientDirectory = ClientDirectory.fromJson(json);
+		List<RelationalNode> nodes = clientDirectory.toRelationalNodeList(0L);
 		// then
-		assertThat(structure).hasSize(5);
-		for (StructureNode node : structure) {
-			assertThat(node).hasNoNullFieldsOrPropertiesExcept("parentFolderId");
+		assertThat(nodes).hasSize(5);
+		for (RelationalNode node : nodes) {
+			assertThat(node).hasNoNullFieldsOrProperties();
 		}
+		System.out.println(nodes);
 	}
 
 	@Test
@@ -55,23 +49,26 @@ public class ParserTest {
 		// given
 		String json = ParserTestCase.CASE_OK_LONG;
 		// when
-		List<StructureNode> structure = structureParser.parseFolderStructure(json);
+		ClientDirectory clientDirectory = ClientDirectory.fromJson(json);
+		List<RelationalNode> nodes = clientDirectory.toRelationalNodeList(0L);
 		// then
-		assertThat(structure).hasSize(5);
-		for (StructureNode node : structure) {
-			assertThat(node).hasNoNullFieldsOrPropertiesExcept("parentFolderId");
+		assertThat(nodes).hasSize(5);
+		for (RelationalNode node : nodes) {
+			assertThat(node).hasNoNullFieldsOrProperties();
 		}
+		System.out.println(nodes);
 	}
 
 	@Test
-	@DisplayName("빈 배열이 들어오면 정상적으로 처리, 빈 배열 반환")
+	@DisplayName("root에 빈 배열이 들어오면 정상적으로 처리, 빈 배열 반환")
 	public void parse_json_no_folder() {
 		// given
-		String json = ParserTestCase.CASE_OK_EMPTY_ARRAY;
+		String json = ParserTestCase.CASE_OK_EMPTY_ARRAY_OR_NULL;
 		// when
-		List<StructureNode> structure = structureParser.parseFolderStructure(json);
+		ClientDirectory clientDirectory = ClientDirectory.fromJson(json);
+		List<RelationalNode> nodes = clientDirectory.toRelationalNodeList(0L);
 		// then
-		assertThat(structure).isEmpty();
+		assertThat(nodes).isEmpty();
 	}
 
 
@@ -80,79 +77,112 @@ public class ParserTest {
 	class WrongJsonStructureExceptionTest {
 
 		@Test
-		@DisplayName("잘못된 구조/정보가 입력될 경우")
+		@DisplayName("잘못된 구조/정보가 입력될 경우 - A")
 		public void parse_json_wrong_format_1() {
 			// given
 			String json = ParserTestCase.CASE_INVALID_A;
 			// when + then
 			assertThatExceptionOfType(ApiFolderException.class).isThrownBy(
-				() -> structureParser.parseFolderStructure(json)
+				() -> ClientDirectory.fromJson(json)
 			);
 		}
 
 		@Test
-		@DisplayName("잘못된 구조/정보가 입력될 경우")
+		@DisplayName("잘못된 구조/정보가 입력될 경우 - B")
 		public void parse_json_wrong_format_2() {
 			// given
 			String json = ParserTestCase.CASE_INVALID_B;
 			// when + then
 			assertThatExceptionOfType(ApiFolderException.class).isThrownBy(
-				() -> structureParser.parseFolderStructure(json)
+				() -> ClientDirectory.fromJson(json)
 			);
 		}
 
 		@Test
-		@DisplayName("잘못된 구조/정보가 입력될 경우")
+		@DisplayName("잘못된 구조/정보가 입력될 경우 - C")
 		public void parse_json_wrong_format_3() {
 			// given
 			String json = ParserTestCase.CASE_INVALID_C;
 			// when + then
 			assertThatExceptionOfType(ApiFolderException.class).isThrownBy(
-				() -> structureParser.parseFolderStructure(json)
+				() -> ClientDirectory.fromJson(json)
 			);
 		}
 
 		@Test
-		@DisplayName("잘못된 구조/정보가 입력될 경우")
+		@DisplayName("잘못된 구조/정보가 입력될 경우 - D")
 		public void parse_json_wrong_format_4() {
 			// given
 			String json = ParserTestCase.CASE_INVALID_D;
 			// when + then
 			assertThatExceptionOfType(ApiFolderException.class).isThrownBy(
-				() -> structureParser.parseFolderStructure(json)
+				() -> ClientDirectory.fromJson(json)
 			);
 		}
 
 		@Test
-		@DisplayName("잘못된 구조/정보가 입력될 경우")
+		@DisplayName("잘못된 구조/정보가 입력될 경우 - E")
 		public void parse_json_wrong_format_5() {
 			// given
 			String json = ParserTestCase.CASE_INVALID_E;
 			// when + then
 			assertThatExceptionOfType(ApiFolderException.class).isThrownBy(
-				() -> structureParser.parseFolderStructure(json)
+				() -> ClientDirectory.fromJson(json)
 			);
 		}
 
 		@Test
-		@DisplayName("잘못된 구조/정보가 입력될 경우")
+		@DisplayName("잘못된 구조/정보가 입력될 경우 - F")
 		public void parse_json_wrong_format_6() {
 			// given
 			String json = ParserTestCase.CASE_INVALID_F;
 			// when + then
 			assertThatExceptionOfType(ApiFolderException.class).isThrownBy(
-				() -> structureParser.parseFolderStructure(json)
+				() -> ClientDirectory.fromJson(json)
 			);
 		}
 
 		@Test
-		@DisplayName("잘못된 구조/정보가 입력될 경우")
+		@DisplayName("잘못된 구조/정보가 입력될 경우 - G")
 		public void parse_json_wrong_format_7() {
 			// given
 			String json = ParserTestCase.CASE_INVALID_G;
 			// when + then
 			assertThatExceptionOfType(ApiFolderException.class).isThrownBy(
-				() -> structureParser.parseFolderStructure(json)
+				() -> ClientDirectory.fromJson(json)
+			);
+		}
+
+		@Test
+		@DisplayName("잘못된 구조/정보가 입력될 경우 - H")
+		public void parse_json_wrong_format_8() {
+			// given
+			String json = ParserTestCase.CASE_INVALID_H;
+			// when + then
+			assertThatExceptionOfType(ApiFolderException.class).isThrownBy(
+				() -> ClientDirectory.fromJson(json)
+			);
+		}
+
+		@Test
+		@DisplayName("잘못된 구조/정보가 입력될 경우 - I")
+		public void parse_json_wrong_format_9() {
+			// given
+			String json = ParserTestCase.CASE_INVALID_I;
+			// when + then
+			assertThatExceptionOfType(ApiFolderException.class).isThrownBy(
+				() -> ClientDirectory.fromJson(json)
+			);
+		}
+
+		@Test
+		@DisplayName("잘못된 구조/정보가 입력될 경우 - J")
+		public void parse_json_wrong_format_10() {
+			// given
+			String json = ParserTestCase.CASE_INVALID_J;
+			// when + then
+			assertThatExceptionOfType(ApiFolderException.class).isThrownBy(
+				() -> ClientDirectory.fromJson(json)
 			);
 		}
 	}
