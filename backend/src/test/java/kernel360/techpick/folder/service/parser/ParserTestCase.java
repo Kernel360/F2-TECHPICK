e4ff60bd8ff55eb872006025a29c39b8cd50b5d4
kimminkyeu <minkyeki@student.42seoul.kr>
2024-10-05 21:52:34 +0900

    :recycle: refactor: 테스트 케이스 최종 수정

--- backend/src/test/java/kernel360/techpick/folder/service/parser/ParserTestCase.java
+++ backend/src/test/java/kernel360/techpick/folder/service/parser/ParserTestCase.java
@@ -2,73 +2,193 @@ package kernel360.techpick.folder.service.parser;
 
 public class ParserTestCase {
 
-	// json 자체가 배열이 아닌 경우
-	public static final String CASE_INVALID_A = "{}";
-	// json 자체가 잘못된 경우
-	public static final String CASE_INVALID_B = "[{]";
+	/**
+	 * 3개의 폴더에 대한 예외 케이스
+	 */
+	// json 에 3가지 폴더 타입이 모두 없는 경우
+	public static final String CASE_INVALID_A = """
+	{ "root": [], "recycleBin": [] }";
+	""";
+	// 미분류 폴더에 하위 폴더가 포함 되어 있는 경우
+	public static final String CASE_INVALID_B = """
+		{
+			"root": [],
+			"recycleBin": [
+				 { "id":"1", "type":"folder", "folderId":1, "name":"폴더", "children": null },
+			],
+			unclassified: []
+		}
+		""";
+	// json 자체가 배열로 들어 오는 경우
+	public static final String CASE_INVALID_C = """
+		[
+			"root": { "id":"1", "type":"folder", "folderId":1, "name":"폴더", "children": null },
+			recycleBin: [],
+			unclassified: []
+		]
+	""";
+	// null 값이 들어 있는 경우
+	public static final String CASE_INVALID_D = """
+		{
+		    "root": [
+		        {
+		            "id": null,
+		            "type": "folder",
+		            "folderId": null,
+		            "name": null,
+		            "children": null
+		        },
+		    ],
+		    "recycleBin": [],
+		    "unclassified": []
+		}
+		""";
+	// null 이 들어 있을 경우
+	public static final String CASE_INVALID_E = """
+		{ "root": null, "recycleBin": [], "unclassified": [] }
+		""";
 	// 빈 정보일 경우
-	public static final String CASE_INVALID_C = "[{}]";
+	public static final String CASE_INVALID_F = """
+		{
+		    "root": [
+		        {},
+		    ],
+		    "recycleBin": [],
+		    "unclassified": [],
+		}
+		""";
 	// 필요한 정보가 타입이 잘못된 경우 (TYPE = FOLDER | PICK)
-	public static final String CASE_INVALID_D = "{id:\"STR\", type:XXXX, folderId:1, name:\"폴더\", children: null}";
+	public static final String CASE_INVALID_G = """
+	   	{
+		    "root": [
+		        {
+		            "id": "hello",
+		            "type": "SOME_WRONG_TYPE",
+		            "folderId": 2,
+		            "name": "some_name",
+		            "children": []
+		        },
+		    ],
+		    "recycleBin": [],
+		    "unclassified": []
+		}
+	""";
 	// 필요한 정보가 타입이 잘못된 경우 (children != array)
-	public static final String CASE_INVALID_E = "{id:\"STR\", type:folder, folderId:1, name:\"폴더\", children: {foo:bar}}";
+	public static final String CASE_INVALID_H = """
+		{
+		    "root": [
+		        {
+		            "id": "hello",
+		            "type": "folder",
+		            "folderId": 2,
+		            "name": "some_name",
+		            "children": { "id: "foo" }
+		        },
+		    ],
+		    "recycleBin": [],
+		    "unclassified": []
+		}
+	""";
 	// id 값이 문자 열이 아닌 경우 (프론트엔드가 string으로 처리)
-	public static final String CASE_INVALID_F = "{id:3, type:XXXX, folderId:1, name:\"폴더\", children: {foo:bar}}";
-	// 필요한 정보가 일부 없는 경우 (type이 없음)
-	public static final String CASE_INVALID_G = "{id:\"STR\", folderId:1, children: null}";
-	// 빈 배열은 OK
-	public static final String CASE_OK_EMPTY_ARRAY = "[]";
+	public static final String CASE_INVALID_I = """
+		{
+		    "root": [
+		        {
+		            "id": 12,
+		            "type": "folder",
+		            "folderId": 2,
+		            "name": "some_name",
+		            "children": []
+		        },
+		    ],
+		    "recycleBin": [],
+		    "unclassified": []
+		}
+		""";
+	// 필요한 정보가 일부 없는 경우 ( type 이 없음 )
+	public static final String CASE_INVALID_J = """
+		{
+		    "root": [
+		        {
+		            "id": 12,
+		            "folderId": 2,
+		            "name": "some_name",
+		            "children": []
+		        },
+		    ],
+		    "recycleBin": [],
+		    "unclassified": []
+		}
+		""";
+
+	/**
+	 * 정상 테스트 케이스
+	 */
+
+	// 정상 케이스 0 - 빈 배열은 OK
+	public static final String CASE_OK_EMPTY_ARRAY_OR_NULL = """
+		{"root": [], "recycleBin": [], "unclassified": []}
+		""";
+
 	// 정상 케이스 1
 	public static final String CASE_OK_SIMPLE = """
-		 [
-		 	{id:"1", type:folder, folderId:1, name:"폴더", children: null},
-			{id:"2", type:folder, folderId:2, name:"폴더", children: null},
-			{id:"3", type:folder, folderId:3, name:"폴더", children: [
-					{id:4, type:folder, folderId:4, name:"폴더", children: []},
-					{id:5, type:folder, folderId:5, name:"폴더", children: null},
-			]}
-		 ]
+		{
+		   "root": [
+		     { "id":"1", "type":"folder", "folderId":1, "name":"폴더", "children": [] },
+		     { "id":"2", "type":"folder", "folderId":2, "name":"폴더", "children": [] },
+		     { "id":"3", "type":"folder", "folderId":3, "name":"폴더", "children": [
+		       { "id":"4", "type":"folder", "folderId":4, "name":"폴더", "children": [] },
+		       { "id":"5", "type":"folder", "folderId":5, "name":"폴더", "children": [] }
+		     ] }
+		   ],
+		   "recycleBin": [],
+		   "unclassified": []
+		 }
 	""";
 	// 정상 케이스 2
 	public static final String CASE_OK_LONG = """
-		[
-		   {
-		     "id": "1",
-		     "type": "folder",
-		     "folderId": 1,
-		     "name": "Favorites",
-		     "children": null
-		   },
-		   {
-		     "id": "2",
-		     "type": "folder",
-		     "folderId": 2,
-		     "name": "Frontend",
-		     "children": [
-		       {
-		         "id": "3",
-		         "type": "folder",
-		         "folderId": 3,
-		         "name": "React",
-		         "children": [
-		           {
-		             "id": "4",
-		             "type": "pick",
-		             "pickId": 4,
-		             "name": "React Hooks"
-		           }
-		         ]
-		       },
-		       {
-		         "id": "5",
-		         "type": "folder",
-		         "folderId": 5,
-		         "name": "TypeScript",
-		         "children": null
-		       }
-		     ]
-		   }
-		 ]
+		{
+		     "root": [
+		         {
+		             "id": "1",
+		             "type": "folder",
+		             "folderId": 1,
+		             "name": "Favorites",
+		             "children": []
+		         },
+		         {
+		             "id": "2",
+		             "type": "folder",
+		             "folderId": 2,
+		             "name": "Frontend",
+		             "children": [
+		                 {
+		                     "id": "3",
+		                     "type": "folder",
+		                     "folderId": 3,
+		                     "name": "React",
+		                     "children": [
+		                         {
+		                             "id": "4",
+		                             "type": "pick",
+		                             "pickId": 4,
+		                             "name": "React Hooks"
+		                         }
+		                     ]
+		                 },
+		                 {
+		                     "id": "5",
+		                     "type": "folder",
+		                     "folderId": 5,
+		                     "name": "TypeScript",
+		                     "children": []
+		                 }
+		             ]
+		         }
+		     ],
+		     "recycleBin": [],
+		     "unclassified": []
+		 }
 	""";
 
 }
