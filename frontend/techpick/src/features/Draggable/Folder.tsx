9913116a033b4f559db626d073f36051f07471d7
Jason <enigsuss@gmail.com>
2024-10-05 15:25:27 +0900

    feat: 폴더 클릭 시 해당 폴더로 이동

--- frontend/techpick/src/features/Draggable/Folder.tsx
+++ frontend/techpick/src/features/Draggable/Folder.tsx
@@ -3,14 +3,21 @@ import Image from 'next/image';
 import { folderWrapper } from '@/features/Draggable/folder.css';
 import { NodeApi } from 'react-arborist';
 import { useDragHook } from '@/hooks/useDragHook';
+import { useTreeStore } from '@/shared/stores/treeStore';
 
 export function Folder({ node }: { node: NodeApi }) {
   const ref = useDragHook(node);
+  const { setFocusedNode } = useTreeStore();
 
   return (
     <div
       ref={ref as unknown as React.LegacyRef<HTMLDivElement>}
       className={folderWrapper}
+      onClick={() => {
+        node.tree.open(node.id);
+        setFocusedNode(node);
+        node.tree.select(node.id);
+      }}
     >
       <Image src={`image/ic_folder.svg`} alt="folder" width={64} height={64} />
       {node.data.name}
