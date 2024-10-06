8243fb34bbdb99d1249a96e3ffb0cb392d011d08
kimminkyeu <minkyeki@student.42seoul.kr>
2024-10-07 04:16:20 +0900

    :sparkles: feat: 최종 Structure 완료

---
+++ backend/src/main/java/kernel360/techpick/feature/structure/controller/StructureController.java
@@ -0,0 +1,8 @@
+package kernel360.techpick.feature.structure.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StructureController {
+    // TODO: implement later
+}
