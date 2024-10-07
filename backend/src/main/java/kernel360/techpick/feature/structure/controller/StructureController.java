aa7cf801599a863a99c2b479ad5d4b1b8500679c
PSH <gyak404@gmail.com>
2024-10-07 18:54:40 +0900

    refactor: Structure API 구현

--- backend/src/main/java/kernel360/techpick/feature/structure/controller/StructureController.java
+++ backend/src/main/java/kernel360/techpick/feature/structure/controller/StructureController.java
@@ -1,8 +1,72 @@
 package kernel360.techpick.feature.structure.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kernel360.techpick.feature.structure.service.StructureService;
+import kernel360.techpick.feature.structure.service.dto.StructureDeleteRequest;
+import kernel360.techpick.feature.structure.service.dto.StructureMoveRequest;
+import lombok.RequiredArgsConstructor;
+
 @RestController
-public class StructureController {
-    // TODO: implement later
+@RequiredArgsConstructor
+@RequestMapping("/api/structures")
+public class StructureController implements StructureApi {
+
+	private final StructureService structureService;
+
+	@Override
+	@GetMapping
+	public ResponseEntity<String> getStructureByUserId(Authentication auth) {
+
+		return ResponseEntity.ok(structureService.getStructureByUserId((Long)auth.getPrincipal()));
+	}
+
+	@Override
+	@PutMapping("/folders/{folderId}")
+	public ResponseEntity<Void> moveFolder(Authentication auth, @PathVariable Long folderId,
+		StructureMoveRequest request) {
+
+		request.updateUserIdAndTargetId(auth, folderId);
+		structureService.moveFolder(request);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@Override
+	@DeleteMapping("/folders/{folderId}")
+	public ResponseEntity<Void> deleteFolder(Authentication auth, @PathVariable Long folderId,
+		StructureDeleteRequest request) {
+
+		request.updateUserIdAndTargetId(auth, folderId);
+		structureService.deleteFolder(request);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@Override
+	@PutMapping("/picks/{pickId}")
+	public ResponseEntity<Void> movePick(Authentication auth, @PathVariable Long pickId,
+		StructureMoveRequest request) {
+
+		request.updateUserIdAndTargetId(auth, pickId);
+		structureService.movePick(request);
+		return ResponseEntity.noContent().build();
+	}
+
+	@Override
+	@DeleteMapping("/picks/{pickId}")
+	public ResponseEntity<Void> deletePick(Authentication auth, @PathVariable Long pickId,
+		StructureDeleteRequest request) {
+
+		request.updateUserIdAndTargetId(auth, pickId);
+		structureService.deletePick(request);
+		return ResponseEntity.noContent().build();
+	}
 }
