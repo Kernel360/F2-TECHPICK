ff99571b24c2f0286748b27b34f6def1893d9e5e
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-10-08 18:38:31 +0900

    feat: 이미지 로딩 시 애니메이션 적용

--- frontend/techpick-extension/src/pages/BookmarkPage/ThumbnailImage.tsx
+++ frontend/techpick-extension/src/pages/BookmarkPage/ThumbnailImage.tsx
@@ -1,13 +1,26 @@
-import { imageStyle, imagePlaceholderStyle } from './ThumbnailImage.css';
+import {
+  imageStyle,
+  imagePlaceholderStyle,
+  fadeInStyle,
+} from './ThumbnailImage.css';
 
 export function ThumbnailImage({ image }: ThumbnailImageProps) {
-  return image ? (
-    <img src={image} alt="Open Graph Image" className={imageStyle} />
-  ) : (
-    <div className={imagePlaceholderStyle} />
+  return (
+    <>
+      {image ? (
+        <img
+          src={image}
+          alt="Bookmark page image"
+          className={`${imageStyle} ${fadeInStyle}`} // 로딩 중에는 fadeInStyle 적용
+        />
+      ) : (
+        <div className={imagePlaceholderStyle} />
+      )}
+    </>
   );
 }
 
+// ThumbnailImageProps 인터페이스 정의
 interface ThumbnailImageProps {
   image?: string | null;
 }
