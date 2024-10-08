59c3658f9f2b56ae3e443361da33a470a7dad35c
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-10-08 12:04:56 +0900

    feat: 로그인 여부 파악 후 페이지 렌더링

---
+++ frontend/techpick-extension/src/features/auth/checkLoginStatus/index.ts
@@ -0,0 +1,1 @@
+export { LoginGuard } from './ui/LoginGuard';
--- frontend/techpick-extension/src/routers/index.tsx
+++ frontend/techpick-extension/src/routers/index.tsx
@@ -1,10 +1,15 @@
 import { createMemoryRouter } from 'react-router-dom';
+import { LoginGuard } from '@/features/auth';
 import { BookmarkPage, LoginPage } from '@/pages';
 
 export const router = createMemoryRouter([
   {
     path: '/',
-    element: <BookmarkPage />,
+    element: (
+      <LoginGuard>
+        <BookmarkPage />
+      </LoginGuard>
+    ),
   },
   {
     path: '/login',
