f0ebb517f7e118991e53da6bc7e81b2b1f95f17c
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-10-08 14:48:15 +0900

    fix: 상수대신 .env 직접 적용
    - 해당 코드에서 에러가 생겨서 추후 수정할 예정

--- frontend/techpick-extension/src/shared/api/index.ts
+++ frontend/techpick-extension/src/shared/api/index.ts
@@ -1,8 +1,7 @@
 import ky from 'ky';
-import { API_URL } from '@/shared';
 
 export let api = ky.create({
-  prefixUrl: API_URL,
+  prefixUrl: import.meta.env.VITE_PUBLIC_API,
   headers: { 'content-type': 'application/json', credentials: 'include' },
 });
 
