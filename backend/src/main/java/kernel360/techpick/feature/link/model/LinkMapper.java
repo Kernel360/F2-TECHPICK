58a413c8d9e153d59de716f4e97eef96a7716ecf
kimminkyeu <minkyeki@student.42seoul.kr>
2024-10-09 22:11:33 +0900

    :recycle: refactor: Link 도메인 리팩토링

--- backend/src/main/java/kernel360/techpick/feature/link/model/LinkMapper.java
+++ backend/src/main/java/kernel360/techpick/feature/link/model/LinkMapper.java
@@ -1,28 +1,16 @@
 package kernel360.techpick.feature.link.model;
 
-import org.springframework.stereotype.Component;
-
 import kernel360.techpick.core.model.link.Link;
 import kernel360.techpick.feature.link.service.dto.LinkRequest;
 import kernel360.techpick.feature.link.service.dto.LinkResponse;
-import kernel360.techpick.feature.pick.service.dto.PickCreateRequest;
-import lombok.RequiredArgsConstructor;
 
-@Component
-@RequiredArgsConstructor
 public class LinkMapper {
 
-	public Link createLink(LinkRequest linkRequest) {
+	public static Link toLinkEntity(LinkRequest linkRequest) {
 		return Link.create(linkRequest.title(), linkRequest.description(), linkRequest.url());
 	}
 
-	public Link createLink(PickCreateRequest pickCreateRequest) {
-		return Link.create(pickCreateRequest.linkRequest().url(), pickCreateRequest.linkRequest().title(),
-			pickCreateRequest.linkRequest().description(),
-			pickCreateRequest.linkRequest().imageUrl());
-	}
-
-	public LinkResponse createLinkResponse(Link link) {
+	public static LinkResponse toLinkResponse(Link link) {
 		return new LinkResponse(
 			link.getId(),
 			link.getUrl(),
--- backend/src/main/java/kernel360/techpick/feature/link/service/LinkService.java
+++ backend/src/main/java/kernel360/techpick/feature/link/service/LinkService.java
@@ -7,7 +7,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import kernel360.techpick.feature.link.exception.ApiLinkException;
 import kernel360.techpick.core.model.link.Link;
-import kernel360.techpick.feature.link.service.dto.LinkRequest;
 import kernel360.techpick.feature.link.service.dto.LinkResponse;
 import kernel360.techpick.feature.link.model.LinkMapper;
 import kernel360.techpick.feature.link.model.LinkProvider;
@@ -18,64 +17,34 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LinkService {
 
-	private final LinkMapper linkMapper;
 	private final LinkProvider linkProvider;
 	private final PickRepository pickRepository;
-
-	/**
-	 * 주어진 URL이 데이터베이스에 존재하지 않는 경우 새로운 {@code Link}를 생성
-	 *
-	 * @param request {@code LinkRequest}
-	 * @throws ApiLinkException [LI-002] 이미 존재하는 링크(URL)
-	 */
-	@Transactional
-	public LinkResponse createLink(LinkRequest request) throws ApiLinkException {
-
-		Link link = linkMapper.createLink(request);
-		validateUrl(request.url());
-
-		return linkMapper.createLinkResponse(linkProvider.save(link));
-	}
 
 	@Transactional(readOnly = true)
 	public LinkResponse getLinkById(Long id) throws ApiLinkException {
 
-		Link link = linkProvider.findById(id);
-
-		return linkMapper.createLinkResponse(link);
+		return LinkMapper.toLinkResponse(linkProvider.findById(id));
 	}
 
 	@Transactional(readOnly = true)
 	public LinkResponse getLinkByUrl(String url) throws ApiLinkException {
 
-		Link link = linkProvider.findByUrl(url);
-
-		return linkMapper.createLinkResponse(link);
+		return LinkMapper.toLinkResponse(linkProvider.findByUrl(url));
 	}
 
 	@Transactional(readOnly = true)
 	public List<LinkResponse> getLinkAll() {
 
-		List<Link> linkList = linkProvider.findAll();
-
-		return linkList.stream()
-			.map(linkMapper::createLinkResponse)
+		return linkProvider.findAll().stream()
+			.map(LinkMapper::toLinkResponse)
 			.toList();
 	}
 
 	@Transactional
 	public void deleteLinkById(Long id) throws ApiLinkException {
 
-		Link targetLink = linkProvider.findById(id);
-		validateLinkHasPick(targetLink);
+		validateLinkHasPick(linkProvider.findById(id));
 		linkProvider.deleteById(id);
-	}
-
-	private void validateUrl(String url) throws ApiLinkException {
-
-		if (linkProvider.existsByUrl(url)) {
-			throw ApiLinkException.LINK_ALREADY_EXISTS();
-		}
 	}
 
 	private void validateLinkHasPick(Link link) throws ApiLinkException {
