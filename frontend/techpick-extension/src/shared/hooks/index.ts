ece3fe830a37662556c23da8a719ed831447e6f9
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-10-09 15:55:06 +0900

    feat: 위, 아래 화살표에 따라 focus를 변경하는 hook 생성

---
+++ frontend/techpick-extension/src/shared/hooks/index.ts
@@ -0,0 +1,1 @@
+export { useChangeFocusUsingArrowKey } from './useChangeFocusUsingArrowKey';
--- frontend/techpick-extension/src/shared/index.ts
+++ frontend/techpick-extension/src/shared/index.ts
@@ -1,3 +1,4 @@
 export * from './api';
 export * from './ui';
 export * from './constants';
+export * from './hooks';
