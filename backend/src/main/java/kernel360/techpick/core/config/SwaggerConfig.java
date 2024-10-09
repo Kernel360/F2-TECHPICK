f765b8b91e1a74bd8907a951c5a917458c4cb52c
Gyaak <gyak404@gmail.com>
2024-10-09 18:40:22 +0900

    [HOTFIX] swagger 쿠키 설정 수정 (#179)
    * refactor: access_token 사용하도록 설정
    
    * fix: swagger 쿠키 설정 수정

--- backend/src/main/java/kernel360/techpick/core/config/SwaggerConfig.java
+++ backend/src/main/java/kernel360/techpick/core/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package kernel360.techpick.core.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
@@ -13,7 +14,10 @@ public class SwaggerConfig {
 	@Bean
 	public OpenAPI openAPI() {
 		return new OpenAPI()
-			.info(apiInfo());
+			.info(apiInfo())
+			.components(new Components()
+				.addSecuritySchemes("basicAuth", securityScheme())
+			);
 	}
 
 	private Info apiInfo() {
