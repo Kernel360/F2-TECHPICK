2b53ab124480caa36b420f88317c0c213f092ff0
kimminkyeu <minkyeki@student.42seoul.kr>
2024-10-12 03:19:40 +0900

    :recycle: refactor: 미사용 import  제거

--- backend/src/main/java/kernel360/techpick/feature/structure/controller/StructureController.java
+++ backend/src/main/java/kernel360/techpick/feature/structure/controller/StructureController.java
@@ -1,6 +1,5 @@
 package kernel360.techpick.feature.structure.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
--- backend/src/main/java/kernel360/techpick/feature/tag/controller/TagController.java
+++ backend/src/main/java/kernel360/techpick/feature/tag/controller/TagController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.Valid;
 import kernel360.techpick.feature.tag.service.dto.TagCreateRequest;
 import kernel360.techpick.feature.tag.service.dto.TagResponse;
 import kernel360.techpick.feature.tag.service.dto.TagUpdateRequest;
