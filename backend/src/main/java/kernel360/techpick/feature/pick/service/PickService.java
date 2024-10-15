e9c151aa8b1ce68c78cf7cb03f176013d493d262
Sangwon Yang <sangwonsheep@gmail.com>
2024-10-15 14:11:04 +0900

    [REFACTOR] url로 Pick 조회 시 리턴 타입 변경 (#223)
    * refactor: url로 Pick 조회 시 리턴 타입 long에서 pickReponse로 변경
    
    * refactor: 리뷰 수정사항 반영

--- backend/src/main/java/kernel360/techpick/feature/pick/service/PickService.java
+++ backend/src/main/java/kernel360/techpick/feature/pick/service/PickService.java
@@ -1,7 +1,6 @@
 package kernel360.techpick.feature.pick.service;
 
 import java.util.List;
-import java.util.Objects;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -77,11 +76,20 @@ public class PickService {
 		return pickMapper.toPickResponseList(picks, pickTagProvider, tagMapper, linkMapper);
 	}
 
+	// Url로 픽 상세 조회
 	@Transactional(readOnly = true)
-	public Long getPickIdByUrl(String url) {
+	public PickResponse getPickByUrl(String url) {
 		User user = userService.getCurrentUser();
 		Pick pick = pickProvider.getByUserAndLinkUrl(user, url);
-		return pick.getId();
+
+		// 본인 픽인지 검증 (pickId)
+		pickValidator.validatePickAccess(userService.getCurrentUser(), pick);
+
+		List<PickTag> pickTagList = pickTagProvider.findAllPickTagByPickId(pick.getId());
+		LinkUrlResponse linkUrlResponse = linkMapper.toLinkUrlResponse(pick.getLink());
+		List<TagResponse> tagResponseList = tagMapper.toTagResponse(pickTagList);
+
+		return pickMapper.toPickResponse(pick, tagResponseList, linkUrlResponse);
 	}
 
 	// 픽 생성
