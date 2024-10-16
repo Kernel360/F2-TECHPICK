27ba04ac6723f6e9cf31ea0cbc8f2e2cf4f52006
PSH <gyak404@gmail.com>
2024-10-16 15:57:33 +0900

    refactor: 정적 메소드 삭제

--- backend/src/main/java/kernel360/techpick/core/model/link/Link.java
+++ backend/src/main/java/kernel360/techpick/core/model/link/Link.java
@@ -21,23 +21,6 @@ public class Link {
 	@Column(name = "id")
 	private Long id;
 
-	/**
-	 * -------------------------------------------------------------------
-	 * TODO:
-	 * 들어온 URL String을 파싱해서 쿼리를 분리 저장 할지, 통으로 할지 토의 필요합니다.
-	 * 만약 한다면, Spring의 UriComponent Class를 이용하면 될 것 같습니다.
-	 * 지금은 쿼리를 따로 저장해야 할 이유를 못 찾았습니다.
-	 * - origin
-	 * - path
-	 * - query
-	 * - etc...
-	 * 참고: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/util/UriComponents.html
-	 * ---------------------------------------------------------------------
-	 * NOTE:
-	 * 링크에 대한 Title, Description은 프론트엔드에서 로딩합니다.
-	 * - 제목이나 설명은 바뀔 수 있기 때문에 동적으로 그때 가져와야 하며, 굳이 DB에 저장할 필요가 없음
-	 */
-
 	// URL
 	// TODO: VARCHAR 최대 크기를 몇으로 할지 토의 필요합니다.
 	// 		 The index key prefix length limit is 3072 bytes for InnoDB -> VARCHAR(1000) + utf8 = 4000byte
@@ -55,26 +38,13 @@ public class Link {
 	@Column(name = "imageUrl", columnDefinition = "VARCHAR(600)")
 	private String imageUrl;
 
-	// TODO: 엔티티 사용자가 정적 팩토리 메소드로 필요한 함수를 구현 하세요
-	// RSS 또는 관리자가 직접 등록 시 사용
-	public static Link create(String url, String title, String description) {
-		return new Link(url, title, description, null);
-	}
-
-	public static Link create(String url, String title, String description, String imageUrl) {
-		return new Link(url, title, description, imageUrl);
-	}
-
-	public void updateLink(String title, String description, String imageUrl) {
-		this.title = title;
-		this.description = description;
-		this.imageUrl = imageUrl;
-	}
-
 	private Link(String url, String title, String description, String imageUrl) {
 		this.url = url;
 		this.title = title;
 		this.description = description;
 		this.imageUrl = imageUrl;
 	}
+
+	// TODO: 엔티티 사용자가 정적 팩토리 메소드로 필요한 함수를 구현 하세요
+
 }
