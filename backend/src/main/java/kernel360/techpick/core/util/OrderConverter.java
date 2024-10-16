55fc7dde366742ef15e9454a1f5225f1d263e834
PSH <gyak404@gmail.com>
2024-10-16 15:58:11 +0900

    feat: id List <-> String Converter 구현

---
+++ backend/src/main/java/kernel360/techpick/core/util/OrderConverter.java
@@ -0,0 +1,29 @@
+package kernel360.techpick.core.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import jakarta.persistence.AttributeConverter;
+
+public class OrderConverter implements AttributeConverter<List<Long>, String> {
+
+	@Override
+	public String convertToDatabaseColumn(List<Long> idList) {
+		StringBuilder sb = new StringBuilder();
+		for (Long id : idList) {
+			sb.append(id).append(" ");
+		}
+		return sb.toString().trim();
+	}
+
+	@Override
+	public List<Long> convertToEntityAttribute(String s) {
+		List<Long> idList = new ArrayList<>();
+		StringTokenizer st = new StringTokenizer(s);
+		while (st.hasMoreTokens()) {
+			idList.add(Long.parseLong(st.nextToken()));
+		}
+		return idList;
+	}
+}
