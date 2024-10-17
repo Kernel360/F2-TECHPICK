c945743e20efff15edbd3f3172195e4715883adb
kimminkyeu <minkyeki@student.42seoul.kr>
2024-10-17 23:59:44 +0900

    feat: api 명세 반영

--- backend/src/main/java/kernel360/techpick/feature/api/folder/controller/FolderApiSpecification.java
+++ backend/src/main/java/kernel360/techpick/feature/api/folder/controller/FolderApiSpecification.java
@@ -2,56 +2,66 @@ package kernel360.techpick.feature.api.folder.controller;
 
 public interface FolderApiSpecification {
 
-    // [루트 폴더 정보 획득]
+    // [ 루트 폴더 정보 획득 ]
     //    이때, 폴더의 이름 + 자식 구조 1 depth 까지만 결과를 반환.
     //    클라이언트에서 펼처진 폴더에 대한 GET 요청을 연쇄적으로 날릴 것.
     // GET api/folders?type=root
     // -------------------- No Request Body
     // -------------------- Response Body = { name, List of "depth-1 child" folder / pick }
 
-    // [미분류 폴더 정보 획득]
+    // [ 미분류 폴더 정보 획득 ]
     //    이때, 폴더의 이름 + 자식 구조 1 depth 까지만 결과를 반환.
     //    클라이언트에서 펼처진 폴더에 대한 GET 요청을 연쇄적으로 날릴 것.
     // GET api/folders?type=unclassified
     // -------------------- No Request Body
     // -------------------- Response Body = { name, List of "depth-1 child" folder / pick }
 
-    // [재활용 폴더 정보 획득]
+    // [ 재활용 폴더 정보 획득 ]
     //    이때, 폴더의 이름 + 자식 구조 1 depth 까지만 결과를 반환.
     //    클라이언트에서 펼처진 폴더에 대한 GET 요청을 연쇄적으로 날릴 것.
     // GET api/folders?type=recycle
     // -------------------- No Request Body
-    // -------------------- Response Body = { name, List of "depth-1 child" folder / pick }
+    // -------------------- Response Body = {
+    //                                         name,
+    //                                         List of "depth-1 child" folder / pick,
+    //                                       }
 
-    // [타입에 관계 없이 폴더 정보 획득]
+    // [ 타입에 관계 없이 폴더 정보 획득 ]
     //    이때, 폴더의 이름 + 자식 구조 1 depth 까지만 결과를 반환.
     //    클라이언트에서 펼처진 폴더에 대한 GET 요청을 연쇄적으로 날릴 것.
     // GET api/folders/{folder_id}
-    // -------------------- Request Body = { name }
+    // -------------------- No Request Body
     // -------------------- Response Body = { name, List of "depth-1 child" folder / pick }
 
-    // [폴더 정보 수정]
-    // PUT api/folders/{folder_id}
-    // -------------------- Request Body = { name, title, tags ... }
-
-    // [폴더 위치 수정]
-    // PATCH api/folders/{folder_id}
+    // [ 폴더 정보 수정 ]
+    // PATCH api/folders/{folder_id}/data
     // -------------------- Request Body = {
-    //                                       parent_folder_id가 있다면 위치도 변경
-    //                                       destination_position_idx : 0이면 최상단 위치로
+    //                                       name : nullable --> null일 경우 아무 것도 안하며, 빈 문자열일 경우 예외 발생
     //                                      }
-
     // -------------------- No Response Body
-    // 이때, parent_folder_id가 휴지통에 있는 id거나 휴지통 id 일 수 있다.
-    // 휴지통으로 이동하는 것은 삭제로 처리하지 않는다.
 
-    // [폴더 생성] + 위치, 순서도 함께
+    // [ 폴더 위치 수정 - 값이 null일 때 "생성"과 다르게 적용되는 점 주의 ]
+    // PATCH api/folders/{folder_id}/location
+    // -------------------- Request Body = {
+    //                                       parent_folder_id: nullable --> 있다면 해당 폴더로 소속되게, 없으면 변경 없음
+    //                                       order_idx : nullable --> 0이면 폴더중 최상단 위치, 없으면 변경 없음
+    //                                      }
+    // -------------------- No Response Body
+    // * 이때, parent_folder_id가 휴지통에 있는 id거나 휴지통 id 일 수 있다.
+    // * 휴지통으로 이동하는 것은 삭제로 처리하지 않는다.
+
+    // [ 폴더 생성 ]
     // POST api/folders/
     // -------------------- Request Body = {
-    //                                       name : 폴더 이름
-    //                                       parent_folder_id : 생성 위치
-    //                                       destination_position_idx <?> : 0이면 최상단 위치, 없으면 제일 마지막.
-    //                                      }
+    //                                       data: {
+    //                                         name : <!!필수값!!> --> 빈 문자열이거나, null일 경우 예외 발생
+    //                                       },
+    //                                       location: {
+    //                                         parent_folder_id: nullable --> 있다면 해당 폴더로 소속되게, 없으면 root로
+    //                                         order_idx : nullable --> 0이면 폴더중 최상단 위치, 없으면 제일 마지막.
+    //                                       }
+    //                                     }
+    //
     // -------------------- Response Body = { created_folder_id }
 
     // [폴더 삭제 - 휴지통으로 부터 삭제]
--- backend/src/main/java/kernel360/techpick/feature/api/pick/controller/PickApiSpecification.java
+++ backend/src/main/java/kernel360/techpick/feature/api/pick/controller/PickApiSpecification.java
@@ -5,33 +5,46 @@ public interface PickApiSpecification {
     // [ 내가 해당 링크를 픽한 적이 있는지 확인 ]
     // GET api/picks?link={url}
     // -------------------- no request body
-    // -------------------- Response Body = { pick_id<?> }
-    //                                        pick_id가 없으면, pick을 안한 url
+    // -------------------- Response Body = {
+    //                                        pick_id: nullable (null == 픽 안함)
+    //                                      }
 
     // [ 나의 픽 조회 ]
     // GET api/picks/{pick_id}
     // -------------------- Request Body = X
     // -------------------- Response Body = { name, parent_folder_id, 태그 ... 등등 }
 
-    // [ 나의 픽 정보 수정 ]
-    // PUT api/picks/{pick_id}
-    // -------------------- Request Body = { name, title, tags ... }
-
-    // [ 나의 픽 위치 수정 ]
-    // PATCH api/picks/{pick_id}
+    // [ 나의 픽 정보 수정 - null이 아닌 값만 업데이트 ]
+    // PATCH api/picks/data/{pick_id}
     // -------------------- Request Body = {
-    //                                       parent_folder_id가 있다면 위치도 변경
-    //                                       destination_position_idx : 0이면 최상단 위치로
+    //                                       name: nullable,
+    //                                       title: nullable,
+    //                                       tags: nullable
     //                                      }
 
-    // [ 나의 픽 위치 생성 ]
+    // [ 나의 픽 위치 수정 - 값이 null일 때 "생성"과 다르게 적용되는 점 주의 ]
+    // PATCH api/picks/location/{pick_id}
+    // -------------------- Request Body = {
+    //                                       parent_folder_id: nullable --> 있다면 해당 폴더로 소속되게, 없으면 변경 없음
+    //                                       order_idx : nullable --> 0이면 픽들 중 최상단 위치, 없으면 변경 없음
+    //                                      }
+
+    // [ 나의 픽 생성 ]
     // POST api/picks/
     // -------------------- Request Body = {
-    //                                       name : 픽 이름
-    //                                       태그 정보 ...
-    //                                       parent_folder_id : 생성 위치
-    //                                       destination_position_idx <?> : 0이면 최상단 위치, 없으면 제일 마지막.
+    //                                       data: {
+    //                                         title: nullable, --> null일 경우 빈 문자열로 설정
+    //                                         memo: nullable,  --> null일 경우 빈 문자열로 설정
+    //                                         tags: nullable,  --> null일 경우 빈 태그로 설정
+    //                                         url: <!!필수값!!>, --> null일 경우 예외 발생
+    //                                         ...
+    //                                       },
+    //                                       location: {
+    //                                         parent_folder_id: nullable --> 있다면 해당 폴더로 소속되게, 없으면 unclassified로
+    //                                         order_idx : nullable --> 0이면 픽들 중 최상단 위치, 없으면 제일 마지막.
+    //                                       }
     //                                      }
+    // -------------------- Response Body = { created_pick_id }
 
     // [ 나의 픽 삭제 조회 ]
     // GET api/picks/{pick_id}
