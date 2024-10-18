40232240850b6a1c79732dd4257d2d78814b43d9
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-10-18 10:15:26 +0900

    chore: import 순서 변경

--- frontend/techpick/src/entities/pick/api/getPick/getPick.ts
+++ frontend/techpick/src/entities/pick/api/getPick/getPick.ts
@@ -1,6 +1,6 @@
+import { HTTPError } from 'ky';
 import { apiClient, returnErrorFromHTTPError } from '@/shared/api';
 import type { GetPickResponseType } from '../../type';
-import { HTTPError } from 'ky';
 
 export const getPick = async (pickId: number): Promise<GetPickResponseType> => {
   try {
