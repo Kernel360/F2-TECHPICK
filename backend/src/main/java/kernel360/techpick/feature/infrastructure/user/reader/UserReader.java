3c2483f53018745ba0bce13f1ecab3156df8f969
kimminkyeu <minkyeki@student.42seoul.kr>
2024-10-20 01:33:30 +0900

    feat: 유저 infra

--- backend/src/main/java/kernel360/techpick/feature/infrastructure/user/reader/UserReader.java
+++ backend/src/main/java/kernel360/techpick/feature/infrastructure/user/reader/UserReader.java
@@ -1,4 +1,8 @@
 package kernel360.techpick.feature.infrastructure.user.reader;
 
+import kernel360.techpick.core.model.user.User;
+
 public interface UserReader {
+
+    User read(Long userId);
 }
--- backend/src/main/java/kernel360/techpick/feature/infrastructure/user/reader/UserReaderImpl.java
+++ backend/src/main/java/kernel360/techpick/feature/infrastructure/user/reader/UserReaderImpl.java
@@ -1,4 +1,19 @@
 package kernel360.techpick.feature.infrastructure.user.reader;
 
+import org.springframework.stereotype.Component;
+
+import kernel360.techpick.core.model.user.User;
+import kernel360.techpick.core.model.user.UserRepository;
+import kernel360.techpick.feature.domain.user.exception.ApiUserException;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class UserReaderImpl implements UserReader {
+    UserRepository userRepository;
+
+    @Override
+    public User read(Long userId) {
+        return userRepository.findById(userId).orElseThrow(ApiUserException::USER_NOT_FOUND);
+    }
 }
