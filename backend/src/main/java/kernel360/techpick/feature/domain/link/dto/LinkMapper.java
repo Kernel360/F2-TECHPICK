727c670bb1b7d4f36b9141589a781b6cf77101a1
kimminkyeu <minkyeki@student.42seoul.kr>
2024-10-20 01:36:26 +0900

    :sparkles: feat: MapStruct 매퍼 구현

--- backend/src/main/java/kernel360/techpick/feature/domain/link/dto/LinkMapper.java
+++ backend/src/main/java/kernel360/techpick/feature/domain/link/dto/LinkMapper.java
@@ -4,10 +4,13 @@ import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
+import kernel360.techpick.core.model.link.Link;
+
 @Mapper(
     componentModel = "spring",
     injectionStrategy = InjectionStrategy.CONSTRUCTOR,
     unmappedTargetPolicy = ReportingPolicy.ERROR
 )
 public interface LinkMapper {
+    Link of(LinkInfo linkInfo);
 }
--- backend/src/main/java/kernel360/techpick/feature/domain/pick/dto/PickMapper.java
+++ backend/src/main/java/kernel360/techpick/feature/domain/pick/dto/PickMapper.java
@@ -4,10 +4,25 @@ import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
+import kernel360.techpick.core.model.folder.Folder;
+import kernel360.techpick.core.model.link.Link;
+import kernel360.techpick.core.model.pick.Pick;
+import kernel360.techpick.core.model.user.User;
+
 @Mapper(
     componentModel = "spring",
     injectionStrategy = InjectionStrategy.CONSTRUCTOR,
     unmappedTargetPolicy = ReportingPolicy.ERROR
 )
 public interface PickMapper {
+
+    PickResult.Create toCreateResult(Pick pick);
+
+    PickResult.Read toReadResult(Pick pick);
+
+    PickResult.Update toUpdateResult(Pick pick);
+
+    PickResult.Move toMoveResult(Pick pick);
+
+    Pick toEntity(PickCommand.Create command, User user, Folder parentFolder, Link link);
 }
