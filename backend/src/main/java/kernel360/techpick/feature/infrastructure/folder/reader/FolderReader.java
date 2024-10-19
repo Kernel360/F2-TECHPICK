3da628d82fe822439b08dbd825ca2c1fb62eacbc
kimminkyeu <minkyeki@student.42seoul.kr>
2024-10-20 01:35:59 +0900

    :sparkles: feat: Folder 인프라 구현

--- backend/src/main/java/kernel360/techpick/feature/infrastructure/folder/reader/FolderReader.java
+++ backend/src/main/java/kernel360/techpick/feature/infrastructure/folder/reader/FolderReader.java
@@ -1,4 +1,9 @@
 package kernel360.techpick.feature.infrastructure.folder.reader;
 
+import kernel360.techpick.core.model.folder.Folder;
+import kernel360.techpick.core.model.user.User;
+
 public interface FolderReader {
+
+    Folder read(User user, Long folderId);
 }
