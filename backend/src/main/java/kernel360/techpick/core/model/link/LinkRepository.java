921849b9f41972a3f9b8dbba3d25d1635db9ce2e
kimminkyeu <minkyeki@student.42seoul.kr>
2024-10-20 01:34:07 +0900

    feat: JPA Repository 구현

--- backend/src/main/java/kernel360/techpick/core/model/link/LinkRepository.java
+++ backend/src/main/java/kernel360/techpick/core/model/link/LinkRepository.java
@@ -1,6 +1,10 @@
 package kernel360.techpick.core.model.link;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LinkRepository extends JpaRepository<Link, Long> {
+
+    Optional<Link> findByUrl(String url);
 }
