4efe169e53c8122fba0b628f5807b8512ca36b28
PSH <gyak404@gmail.com>
2024-10-21 21:01:48 +0900

    feat: 폴더 repository 자식폴더 조회 메소드 구현

--- backend/src/main/java/kernel360/techpick/core/model/folder/FolderRepository.java
+++ backend/src/main/java/kernel360/techpick/core/model/folder/FolderRepository.java
@@ -1,6 +1,10 @@
 package kernel360.techpick.core.model.folder;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FolderRepository extends JpaRepository<Folder, Long> {
+
+	List<Folder> findByParentFolder(Folder parentFolder);
 }
