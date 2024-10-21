a64a5f273addadd368d178b32e78e9c82a60c447
sangwonsheep <sangwonsheep@gmail.com>
2024-10-21 12:51:53 +0900

    feat: tag infra 구현

--- backend/src/main/java/kernel360/techpick/feature/infrastructure/tag/reader/TagReaderImpl.java
+++ backend/src/main/java/kernel360/techpick/feature/infrastructure/tag/reader/TagReaderImpl.java
@@ -1,4 +1,39 @@
 package kernel360.techpick.feature.infrastructure.tag.reader;
 
+import java.util.List;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.stereotype.Component;
+
+import kernel360.techpick.core.model.tag.Tag;
+import kernel360.techpick.core.model.tag.TagRepository;
+import kernel360.techpick.feature.domain.tag.exception.ApiTagException;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class TagReaderImpl implements TagReader {
+
+	private TagRepository tagRepository;
+
+	@Override
+	public Tag readTag(Long userId, Long tagId) {
+		Tag tag = tagRepository.findById(tagId).orElseThrow(ApiTagException::TAG_NOT_FOUND);
+
+		if (ObjectUtils.notEqual(tag.getUser().getId(), userId)) {
+			ApiTagException.UNAUTHORIZED_TAG_ACCESS();
+		}
+
+		return tag;
+	}
+
+	@Override
+	public List<Tag> readTagList(Long userId) {
+		return tagRepository.findAllByUserId(userId);
+	}
+
+	@Override
+	public boolean checkDuplicateTagName(Long userId, String name) {
+		return tagRepository.existsByUserIdAndName(userId, name);
+	}
 }
--- backend/src/main/java/kernel360/techpick/feature/infrastructure/tag/writer/TagWriterImpl.java
+++ backend/src/main/java/kernel360/techpick/feature/infrastructure/tag/writer/TagWriterImpl.java
@@ -1,4 +1,24 @@
 package kernel360.techpick.feature.infrastructure.tag.writer;
 
+import org.springframework.stereotype.Component;
+
+import kernel360.techpick.core.model.tag.Tag;
+import kernel360.techpick.core.model.tag.TagRepository;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class TagWriterImpl implements TagWriter {
+
+	private final TagRepository tagRepository;
+
+	@Override
+	public Tag writeTag(Tag tag) {
+		return tagRepository.save(tag);
+	}
+
+	@Override
+	public void removeTag(Long tagId) {
+		tagRepository.deleteById(tagId);
+	}
 }
