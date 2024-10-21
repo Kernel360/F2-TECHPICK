f9d742f85f803e8350028d88ee7af7daf4ba4a60
sangwonsheep <sangwonsheep@gmail.com>
2024-10-21 18:07:37 +0900

    feat: tag api request/response 구현

--- backend/src/main/java/kernel360/techpick/feature/api/tag/dto/TagApiRequest.java
+++ backend/src/main/java/kernel360/techpick/feature/api/tag/dto/TagApiRequest.java
@@ -1,9 +1,27 @@
 package kernel360.techpick.feature.api.tag.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 public class TagApiRequest {
 
-    public record Create() {}
-    public record Read() {}
-    public record Update() {}
-    public record Delete() {}
+	public record Create(
+		@NotBlank String name,
+		@NotNull Integer colorNumber) {
+	}
+
+	public record Read(
+		@NotNull Long tagId) {
+	}
+
+	public record Update(
+		@NotNull Long tagId,
+		@NotEmpty String name,
+		@NotNull Integer colorNumber) {
+	}
+
+	public record Delete(
+		@NotNull Long tagId) {
+	}
 }
