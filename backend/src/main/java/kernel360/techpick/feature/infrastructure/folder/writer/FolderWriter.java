fa5f93fd9ca926d362ef890e336925618bfb5f3f
PSH <gyak404@gmail.com>
2024-10-21 21:01:08 +0900

    feat: 폴더 reader/writer 구현

--- backend/src/main/java/kernel360/techpick/feature/infrastructure/folder/writer/FolderWriter.java
+++ backend/src/main/java/kernel360/techpick/feature/infrastructure/folder/writer/FolderWriter.java
@@ -1,4 +1,11 @@
 package kernel360.techpick.feature.infrastructure.folder.writer;
 
+import kernel360.techpick.core.model.folder.Folder;
+import kernel360.techpick.feature.domain.folder.exception.ApiFolderException;
+
 public interface FolderWriter {
+
+	Folder writeFolder(Folder folder) throws ApiFolderException;
+
+	void removeFolder(Folder folder);
 }
