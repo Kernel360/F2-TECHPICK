b940fa9d1b58b6e2f424e9a5c10e3814617071b4
PSH <gyak404@gmail.com>
2024-10-21 11:23:28 +0900

    refactor: 현재 사용자 조회 메소드 구현

--- backend/src/main/java/kernel360/techpick/feature/domain/pick/service/PickServiceImpl.java
+++ backend/src/main/java/kernel360/techpick/feature/domain/pick/service/PickServiceImpl.java
@@ -17,71 +17,71 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class PickServiceImpl implements PickService {
-    private final UserReader userReader;
-    private final PickReader pickReader;
-    private final PickWriter pickWriter;
-    private final PickMapper pickMapper;
-    private final LinkWriter linkWriter;
-    private final FolderReader folderReader;
+	private final UserReader userReader;
+	private final PickReader pickReader;
+	private final PickWriter pickWriter;
+	private final PickMapper pickMapper;
+	private final LinkWriter linkWriter;
+	private final FolderReader folderReader;
 
-    @Override
-    @Transactional(readOnly = true)
-    public PickResult.Read getPick(PickCommand.Read command) {
-        var user = userReader.read(command.userId());
-        var pick = pickReader.read(user, command.pickId());
-        return pickMapper.toReadResult(pick);
-    }
+	@Override
+	@Transactional(readOnly = true)
+	public PickResult.Read getPick(PickCommand.Read command) {
+		var user = userReader.readCurrentUser();
+		var pick = pickReader.read(user, command.pickId());
+		return pickMapper.toReadResult(pick);
+	}
 
-    @Override
-    @Transactional
-    public PickResult.Create saveNewPick(PickCommand.Create command) {
-        var user = userReader.read(command.userId());
-        var folder = folderReader.read(user, command.parentFolderId());
-        var link = linkWriter.write(command.linkInfo());
-        var pick = pickWriter.write(pickMapper.toEntity(command, user, folder, link));
-        return pickMapper.toCreateResult(pick);
-    }
+	@Override
+	@Transactional
+	public PickResult.Create saveNewPick(PickCommand.Create command) {
+		var user = userReader.readCurrentUser();
+		var folder = folderReader.read(user, command.parentFolderId());
+		var link = linkWriter.write(command.linkInfo());
+		var pick = pickWriter.write(pickMapper.toEntity(command, user, folder, link));
+		return pickMapper.toCreateResult(pick);
+	}
 
-    @Override
-    @Transactional
-    public PickResult.Update updatePick(PickCommand.Update command) {
-        var user = userReader.read(command.userId());
-        var pick = pickReader.read(user, command.pickId())
-                             .updateMemo(command.memo())
-                             .updateTagOrder(command.tagIdList())
-                             .updateTitle(command.title());
-        return pickMapper.toUpdateResult(pick);
-    }
+	@Override
+	@Transactional
+	public PickResult.Update updatePick(PickCommand.Update command) {
+		var user = userReader.readCurrentUser();
+		var pick = pickReader.read(user, command.pickId())
+			.updateMemo(command.memo())
+			.updateTagOrder(command.tagIdList())
+			.updateTitle(command.title());
+		return pickMapper.toUpdateResult(pick);
+	}
 
-    @Override
-    @Transactional
-    public PickResult.Move movePick(PickCommand.Move command) {
-        var user = userReader.read(command.userId());
-        var pick = pickReader.read(user, command.pickId());
-        var originalParentFolder = pick.getParentFolder();
+	@Override
+	@Transactional
+	public PickResult.Move movePick(PickCommand.Move command) {
+		var user = userReader.readCurrentUser();
+		var pick = pickReader.read(user, command.pickId());
+		var originalParentFolder = pick.getParentFolder();
 
-        if (isParentFolderNotChanged(command, originalParentFolder)) {
-            originalParentFolder.updateChildPickOrder(command.pickId(), command.orderIdx());
-            return pickMapper.toMoveResult(pick);
-        }
-        originalParentFolder.removeChildPickOrder(command.pickId());
-        var newParentFolder = folderReader.read(user, command.parentFolderId())
-                                          .updateChildPickOrder(command.pickId(), command.orderIdx());
-        pick.updateParentFolder(newParentFolder);
-        return pickMapper.toMoveResult(pick);
-    }
+		if (isParentFolderNotChanged(command, originalParentFolder)) {
+			originalParentFolder.updateChildPickOrder(command.pickId(), command.orderIdx());
+			return pickMapper.toMoveResult(pick);
+		}
+		originalParentFolder.removeChildPickOrder(command.pickId());
+		var newParentFolder = folderReader.read(user, command.parentFolderId())
+			.updateChildPickOrder(command.pickId(), command.orderIdx());
+		pick.updateParentFolder(newParentFolder);
+		return pickMapper.toMoveResult(pick);
+	}
 
-    @Override
-    @Transactional
-    public void deletePick(PickCommand.Delete command) {
-        var user = userReader.read(command.userId());
-        var pick = pickReader.read(user, command.pickId());
-        var folder = pick.getParentFolder();
-        folder.removeChildPickOrder(command.pickId());
-        pickWriter.remove(pick);
-    }
+	@Override
+	@Transactional
+	public void deletePick(PickCommand.Delete command) {
+		var user = userReader.readCurrentUser();
+		var pick = pickReader.read(user, command.pickId());
+		var folder = pick.getParentFolder();
+		folder.removeChildPickOrder(command.pickId());
+		pickWriter.remove(pick);
+	}
 
-    private boolean isParentFolderNotChanged(PickCommand.Move command, Folder originalFolder) {
-        return (command.parentFolderId() == null || originalFolder.getId().equals(command.parentFolderId()));
-    }
+	private boolean isParentFolderNotChanged(PickCommand.Move command, Folder originalFolder) {
+		return (command.parentFolderId() == null || originalFolder.getId().equals(command.parentFolderId()));
+	}
 }
--- backend/src/main/java/kernel360/techpick/feature/infrastructure/user/reader/UserReader.java
+++ backend/src/main/java/kernel360/techpick/feature/infrastructure/user/reader/UserReader.java
@@ -4,5 +4,9 @@ import kernel360.techpick.core.model.user.User;
 
 public interface UserReader {
 
-    User read(Long userId);
+	User readUser(Long userId);
+
+	User readCurrentUser();
+
+	Long readCurrentUserId();
 }
