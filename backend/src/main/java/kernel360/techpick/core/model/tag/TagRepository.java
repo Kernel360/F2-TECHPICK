37e5b25b2d33c9e63582b6f5ad5de0e1211349ca
PSH <gyak404@gmail.com>
2024-10-23 22:53:09 +0900

    refactor: TagRepository 비관락 타임아웃 설정

--- backend/src/main/java/kernel360/techpick/core/model/tag/TagRepository.java
+++ backend/src/main/java/kernel360/techpick/core/model/tag/TagRepository.java
@@ -3,11 +3,16 @@ package kernel360.techpick.core.model.tag;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
+
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
-	// @Lock(LockModeType.PESSIMISTIC_WRITE)
-	// @QueryHints(@QueryHint(name = "jakarta.persistence.lock.timeout", value = "1000"))
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@QueryHints(@QueryHint(name = "jakarta.persistence.lock.timeout", value = "1000"))
 	boolean existsByUserIdAndName(Long userId, String name);
 
 	List<Tag> findAllByUserId(Long userId);
