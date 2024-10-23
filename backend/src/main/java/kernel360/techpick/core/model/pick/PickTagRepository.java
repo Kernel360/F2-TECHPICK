1682ebda9d8a238a90b91e59ad3d01504c969ccc
PSH <gyak404@gmail.com>
2024-10-23 22:46:07 +0900

    refactor: TagAdaptor로 db로직 이관

--- backend/src/main/java/kernel360/techpick/core/model/pick/PickTagRepository.java
+++ backend/src/main/java/kernel360/techpick/core/model/pick/PickTagRepository.java
@@ -1,8 +1,12 @@
 package kernel360.techpick.core.model.pick;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PickTagRepository extends JpaRepository<PickTag, Long> {
+	
+	List<PickTag> findByTagId(Long tagId);
 
 	void deleteByPick(Pick pick);
 
--- backend/src/main/java/kernel360/techpick/core/model/tag/TagRepository.java
+++ backend/src/main/java/kernel360/techpick/core/model/tag/TagRepository.java
@@ -3,13 +3,11 @@ package kernel360.techpick.core.model.tag;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-
-import jakarta.persistence.LockModeType;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
-	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	// @Lock(LockModeType.PESSIMISTIC_WRITE)
+	// @QueryHints(@QueryHint(name = "jakarta.persistence.lock.timeout", value = "1000"))
 	boolean existsByUserIdAndName(Long userId, String name);
 
 	List<Tag> findAllByUserId(Long userId);
