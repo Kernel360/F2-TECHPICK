5c00674f6c09cd08a910ab56ca6a0890a74e03ca
kimminkyeu <minkyeki@student.42seoul.kr>
2024-10-24 15:45:10 +0900

    feat:  request dto에서 userId 제거

--- backend/src/main/java/kernel360/techpick/feature/api/folder/dto/FolderApiMapper.java
+++ backend/src/main/java/kernel360/techpick/feature/api/folder/dto/FolderApiMapper.java
@@ -13,17 +13,17 @@ import kernel360.techpick.feature.domain.folder.dto.FolderResult;
 	unmappedTargetPolicy = ReportingPolicy.ERROR
 )
 public interface FolderApiMapper {
-	FolderCommand.Create toCreateCommand(FolderApiRequest.Create request);
+	FolderCommand.Create toCreateCommand(FolderApiRequest.Create request, Long userId);
 
-	FolderCommand.Read toReadCommand(FolderApiRequest.Read request);
+	FolderCommand.Read toReadCommand(FolderApiRequest.Read request, Long userId);
 
-	FolderCommand.Update toUpdateCommand(FolderApiRequest.Update request);
+	FolderCommand.Update toUpdateCommand(FolderApiRequest.Update request, Long userId);
 
-	FolderCommand.Move toMoveCommand(FolderApiRequest.Move request);
+	FolderCommand.Move toMoveCommand(FolderApiRequest.Move request, Long userId);
 
-	FolderCommand.Delete toDeleteCommand(FolderApiRequest.Delete request);
+	FolderCommand.Delete toDeleteCommand(FolderApiRequest.Delete request, Long userId);
 
-	FolderApiResponse.Create toCreateResponse(FolderResult request);
+	FolderApiResponse.Create toCreateResponse(FolderResult request, Long userId);
 
-	FolderApiResponse.Read toReadResponse(FolderResult request);
+	FolderApiResponse.Read toReadResponse(FolderResult request, Long userId);
 }
